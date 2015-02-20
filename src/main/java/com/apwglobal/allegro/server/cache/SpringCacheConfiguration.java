06a26f4c6243461bb584ef01deda83ddb5aa4066
Artur Wronski <a.wronski@mgmoptima.com>
2015-02-20 17:29:08 +0100

    - cache configuration

---
+++ src/main/java/com/apwglobal/allegro/server/cache/SpringCacheConfiguration.java
@@ -0,0 +1,58 @@
+package com.apwglobal.allegro.server.cache;
+
+import net.sf.ehcache.config.CacheConfiguration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.interceptor.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class SpringCacheConfiguration implements CachingConfigurer {
+
+    @Bean(destroyMethod="shutdown")
+    public net.sf.ehcache.CacheManager ehCacheManager() {
+
+        net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
+        config.addCache(createCacheConfiguration("journal", 1));
+        config.addCache(createCacheConfiguration("auctions", 1));
+
+        return net.sf.ehcache.CacheManager.newInstance(config);
+    }
+
+    private CacheConfiguration createCacheConfiguration(String cacheName, int maxEntries) {
+        CacheConfiguration cacheConfiguration = new CacheConfiguration();
+        cacheConfiguration.setName(cacheName);
+        cacheConfiguration.setMemoryStoreEvictionPolicy("LRU");
+        cacheConfiguration.setMaxEntriesLocalHeap(maxEntries);
+        cacheConfiguration.setEternal(false);
+        cacheConfiguration.setTimeToIdleSeconds(3 * 60);
+        cacheConfiguration.setTimeToLiveSeconds(3 * 60);
+        return cacheConfiguration;
+    }
+
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        return new EhCacheCacheManager(ehCacheManager());
+    }
+
+    @Override
+    public CacheResolver cacheResolver() {
+        return new SimpleCacheResolver(cacheManager());
+    }
+
+    @Bean
+    @Override
+    public KeyGenerator keyGenerator() {
+        return new SimpleKeyGenerator();
+    }
+
+    @Override
+    public CacheErrorHandler errorHandler() {
+        return new SimpleCacheErrorHandler();
+    }
+}

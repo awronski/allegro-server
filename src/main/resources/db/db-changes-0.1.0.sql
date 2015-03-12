3e69405c3f73f97ce99cab6d58e2e3ba170abc2e
Artur Wronski <a.wronski@mgmoptima.com>
2015-03-12 11:29:03 +0100

    - journal table was added

--- src/main/resources/db/db-changes-0.1.0.sql
+++ src/main/resources/db/db-changes-0.1.0.sql
@@ -3,6 +3,7 @@ CREATE USER alle;
 ALTER ROLE alle PASSWORD 'haslo';
 CREATE DATABASE alledb OWNER alle ENCODING = 'UTF-8';
 
+
 DROP TABLE IF EXISTS auctions;
 CREATE TABLE auctions (
   itemId              BIGINT NOT NULL PRIMARY KEY,
@@ -25,3 +26,14 @@ CREATE TABLE auctions (
   priceType           VARCHAR(8) NOT NULL
 );
 ALTER TABLE auctions OWNER TO alle;
+
+
+DROP TABLE IF EXISTS journals;
+CREATE TABLE journals (
+  rowId         BIGINT NOT NULL PRIMARY KEY,
+  itemId        BIGINT NOT NULL,
+  changeType    VARCHAR(10) NOT NULL,
+  changeDate    TIMESTAMP NOt NULL,
+  currentPrice  DOUBLE PRECISION
+);
+ALTER TABLE journals OWNER TO alle;

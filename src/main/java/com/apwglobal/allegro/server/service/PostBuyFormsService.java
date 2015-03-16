fb3e4d92ae2441f0f53418638967c8b8247ed4cb
Artur Wronski <a.wronski@mgmoptima.com>
2015-03-16 11:07:17 +0100

    - logging

--- src/main/java/com/apwglobal/allegro/server/service/PostBuyFormsService.java
+++ src/main/java/com/apwglobal/allegro/server/service/PostBuyFormsService.java
@@ -2,6 +2,8 @@ package com.apwglobal.allegro.server.service;
 
 import com.apwglobal.allegro.server.dao.PostBuyFormDao;
 import com.apwglobal.nice.domain.PostBuyForm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +13,8 @@ import java.util.Optional;
 @Service
 @Transactional
 public class PostBuyFormsService implements IPostBuyFormsService {
+
+    private final static Logger logger = LoggerFactory.getLogger(PostBuyFormsService.class);
 
     @Autowired
     private PostBuyFormDao postBuyFormDao;
@@ -25,6 +29,8 @@ public class PostBuyFormsService implements IPostBuyFormsService {
         saveItems(f);
         saveAddresses(f);
         postBuyFormDao.savePostBuyForm(f);
+
+        logger.debug("Saved: {}", f);
     }
 
     private void saveAddresses(PostBuyForm f) {

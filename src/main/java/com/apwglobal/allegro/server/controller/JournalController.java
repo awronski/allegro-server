22495d80bfb81c4eeaaa673fb876f61e46821076
Artur Wronski <a.wronski@mgmoptima.com>
2015-03-19 10:56:37 +0100

    - search journal via rest controller

--- src/main/java/com/apwglobal/allegro/server/controller/JournalController.java
+++ src/main/java/com/apwglobal/allegro/server/controller/JournalController.java
@@ -1,5 +1,6 @@
 package com.apwglobal.allegro.server.controller;
 
+import com.apwglobal.allegro.server.command.SearchJournal;
 import com.apwglobal.allegro.server.service.IJournalService;
 import com.apwglobal.nice.domain.Journal;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +13,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Controller
 public class JournalController {
+
+    public static final int SEARCH_LIMIT = 100;
 
     @Autowired
     private IJournalService journalService;
@@ -29,8 +36,35 @@ public class JournalController {
     @Cacheable("journals")
     @RequestMapping("/journals")
     @ResponseBody
-    public List<Journal> journal(@RequestParam(value="limit",required=false,defaultValue="50") int limit) {
+    public List<Journal> journal(@RequestParam(value = "limit", required = false, defaultValue = "50") int limit) {
         return journalService.getLastJournals(limit);
+    }
+
+    @RequestMapping("/journals/search")
+    @ResponseBody
+    public List<Journal> search(SearchJournal s) {
+        return journalService.getLastJournals(s.getLimit().orElse(SEARCH_LIMIT))
+                .stream()
+                .filter(j -> testEquals(s.getItemId(), j.getItemId()))
+                .filter(j -> testEquals(s.getRowId(), j.getRowId()))
+                .filter(j -> testEquals(s.getChangeType(), j.getChangeType()))
+                .filter(j -> testGe(s.getFrom(), j.getChangeDate()))
+                .filter(j -> testLe(s.getTo(), j.getChangeDate()))
+                .collect(Collectors.toList());
+    }
+
+    private boolean testEquals(Optional o, Object v) {
+        return !o.isPresent() || o.get().equals(v);
+    }
+
+    @SuppressWarnings("unchecked")
+    private boolean testGe(Optional<? extends Comparable> o, Comparable v) {
+        return !o.isPresent() || v.compareTo(o.get()) >= 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    private boolean testLe(Optional<? extends Comparable> o, Comparable v) {
+        return !o.isPresent() || v.compareTo(o.get()) <= 0;
     }
 
 }

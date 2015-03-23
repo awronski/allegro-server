2331f6952ffa2c8a7c516fecb84b82fdf74724f1
Artur Wronski <a.wronski@mgmoptima.com>
2015-03-23 15:28:05 +0100

    - date format was set

--- src/main/java/com/apwglobal/allegro/server/json/Jdk8ModuleConfiguration.java
+++ src/main/java/com/apwglobal/allegro/server/json/Jdk8ModuleConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.text.SimpleDateFormat;
 
 @Configuration
 public class Jdk8ModuleConfiguration {
@@ -16,6 +17,7 @@ public class Jdk8ModuleConfiguration {
     @PostConstruct
     public void setup() {
         mapper.registerModule(new Jdk8Module());
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ"));
     }
 
 }

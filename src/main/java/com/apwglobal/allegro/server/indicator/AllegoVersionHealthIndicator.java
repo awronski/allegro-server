2e35b7c634059996e3238095e91ddc96ae02fc91
Artur Wronski <a.wronski@mgmoptima.com>
2015-04-14 17:19:01 +0200

    - allegro version health check indicator

---
+++ src/main/java/com/apwglobal/allegro/server/indicator/AllegoVersionHealthIndicator.java
@@ -0,0 +1,31 @@
+package com.apwglobal.allegro.server.indicator;
+
+import com.apwglobal.nice.service.IAllegroNiceApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+import pl.allegro.webapi.SysStatusType;
+
+@Component
+public class AllegoVersionHealthIndicator extends AbstractHealthIndicator {
+
+    @Autowired
+    private IAllegroNiceApi allegro;
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        SysStatusType status = allegro.getStatus();
+
+        builder.withDetail("apiVersion", status.getApiVersion());
+        builder.withDetail("attribVersion", status.getAttribVersion());
+        builder.withDetail("catsVersion", status.getCatsVersion());
+        builder.withDetail("formSellVersion", status.getFormSellVersion());
+        builder.withDetail("programVersion", status.getProgramVersion());
+        builder.withDetail("siteVersion", status.getSiteVersion());
+        builder.withDetail("verKey", status.getVerKey());
+
+        builder.up();
+    }
+
+}
